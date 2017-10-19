d6dfdf75600258b7c11ba895120f82be1c9e0337
han8909227 <hbao2016@hotmail.com>
2017-10-19 14:25:29 -0700

    addded the functionality of making dictionary out of the list we generted and pass the tests for it

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -16,3 +16,18 @@ def test_text_to_list(text, result):
     """Test that book_to_list splits text into list."""
     from trigrams import book_to_list
     assert book_to_list(text) == result
+
+
+DICT_TEST = [(['any', 'thing', 'else'], {'any thing': ['else']}),
+(['I','wish','I','may','I','wish','I','might'],
+{'I wish': ['I', 'I'], 'wish I':['may', 'might'], 'may I':['wish'], 'I may':['I']
+})]
+
+
+@pytest.mark.parametrize('words_list,result', DICT_TEST)
+def test_make_dict(words_list, result):
+    """Make a test with list of words and making a dictionary for two consecutive words as key and third as value"""
+
+    from trigrams import list_to_dict
+    assert list_to_dict(words_list) == result
+
