dcab515be6cadc3e7bc09640ee23df5d766c4b6b
Megan Flood <mak.flood@comcast.net>
2017-10-19 12:58:48 -0700

    Initial setup of package Added a .gitignore and python files for trigrams and testing of trigrams. Also added a setup.py for installing the trigrams package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='trigrams',
+    author='Megan Flood and Han Bao',
+    author_email='hbao2016@hotmail.com',
+    description='Generates a book from another book using trigrams.',
+    package_dir={'': 'src'},
+    py_modules=['trigrams']
+)
