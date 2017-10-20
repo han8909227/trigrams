f1b25aea6fd7e2506a065e049a769d440d5b7c48
eldocwho <fortunato.maycotte@gmail.com>
2017-10-20 14:10:58 -0700

    Finished setup file

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(
     author_email='hbao2016@hotmail.com',
     description='Generates a book from another book using trigrams.',
     package_dir={'': 'src'},
-    py_modules=['trigrams']
+    py_modules=['trigrams'],
+    install_requires=[],
+    extras_require={
+        'test': ['pytest', 'pytest-watch', 'pytest-cov']
+    },
 )
